eef205f3826ac8a7098f0a0640165bd02b333def
Amit Tole <tolecat@gmail.com>
2020-10-13 23:30:41 +0300

    adding first components and styling it

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CtaTop from "./components/ctaTop/CtaTop";
+
+import "./styles/reset.css";
+import "./styles/cta-top.css";
+
+function App() {
+  return <CtaTop />;
+}
+
+export default App;
