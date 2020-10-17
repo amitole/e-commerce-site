72f47e69b0da72b781c07c7b98b863c8138e26bc
Amit Tole <tolecat@gmail.com>
2020-10-17 21:17:49 +0300

    adding footer and route pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/header/Header";
 import Landing from "./pages/Landing";
-
+import Auth from "./pages/Auth";
+import Footer from "./components/footer/Footer";
 
 import "./styles/main.css";
 
 function App() {
-  return <Landing />;
+  return (
+    <Fragment>
+      <Router>
+        <Header />
+        <div className="page-container">
+          <Switch>
+            <Route exect path="/auth">
+              <Auth />
+            </Route>
+            <Route exect path="/">
+              <Landing />;
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from "react";
 import CtaTop from "../components/ctaTop/CtaTop";
-import Header from "../components/header/Header";
 import CourseSlider from "../components/courseSlider/CourseSlider";
 
 const Lending = () => {
   return (
     <Fragment>
-      <Header />
       <CtaTop />
       <CourseSlider />
     </Fragment>
